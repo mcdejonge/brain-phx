8af8357cd747f0da10032d32d9537048f37e5d80
Matthijs de Jonge <rhmtts@gmail.com>
2019-05-06 17:48:51 +0200

    fixed ANOTHER bug and implemented get_file function

--- lib/brain/file_storage.ex
+++ lib/brain/file_storage.ex
@@ -3,6 +3,7 @@ defmodule Brain.FileStorage do
   @moduledoc """
   Module for handling file storage. 
   """
+
   @doc """
   Returns a list of all available files (recursively).
   Each item in the list looks like this:
@@ -10,22 +11,58 @@ defmodule Brain.FileStorage do
   type : the file type (""if it's a directory)
   path : the file system path
   children : items below this item (only relevant for directories - [] for files).
+  TODO : date last modified
 """
-  def get_all(basedir) do
-    get_all_below(basedir, "")
+  def get_all() do
+    basedir = Application.get_env(:brain, BrainWeb.Endpoint)[:content_dir]
+    get_all_below(basedir, basedir)
   end
 
   defp get_all_below(root, startdir) do
-    Enum.map(Path.wildcard("#{root}/#{startdir}/*", [true] ), fn(raw) -> 
-      %{:path => Path.relative_to(raw, root), 
+    Enum.map(Path.wildcard("#{startdir}/*", [true] ), fn(raw) -> 
+      %{
+        :path => Path.relative_to(raw, root), 
         :dir => Path.dirname(raw),
         :type => String.downcase(Regex.replace(~r/^\./, Path.extname(raw), "")),
         :title => Regex.replace(~r/[^a-zA-Z0-9-]+/, Path.basename(raw, Path.extname(raw)), " "),
-        :children => get_all_below("#{startdir}", Path.relative_to(raw, startdir))
+        :children => get_all_below(root, raw)
       }
     end)
     |> Enum.sort(fn a, b -> a.type >= b.type end)
     |> Enum.sort(fn a, b -> a.title <= b.title end)
+  end
+
+
+  @doc """
+  Return data for the requested file. The path is expanded to the full path, ie appended to the base content dir.
+  path : the path to open
+  Returns: Nil if the file does not exist or is a directory, otherwise a map with:
+  title
+  path (relative to content dir)
+  type
+  contents
+  TODO make it a struct
+  """
+  def get_file(path) do
+    full_path = Path.join(Application.get_env(:brain, BrainWeb.Endpoint)[:content_dir], path)
+    cond do
+      ! File.exists?(full_path) -> Nil
+      File.dir?(full_path) -> Nil
+      true -> get_file_verified_to_exist(path)
+        
+    end
+  end
+
+  defp get_file_verified_to_exist(path) do
+    full_path = Path.join(Application.get_env(:brain, BrainWeb.Endpoint)[:content_dir], path)
+    {_, contents} = File.read(full_path)
+    %{
+      :path => path,
+      :title => Regex.replace(~r/[^a-zA-Z0-9-]+/, Path.basename(full_path, Path.extname(full_path)), " "),
+      :type => String.downcase(Regex.replace(~r/^\./, Path.extname(full_path), "")),
+      :contents => contents,
+    }
+
   end
   
 end
