9b8627b6f62ff9e53215ee9e701fb87f340bb181
Matthijs de Jonge <rhmtts@gmail.com>
2019-05-20 11:53:10 +0200

    built simple file indexer / searcher (for one search term)

---
+++ lib/brain/file_find.ex
@@ -0,0 +1,123 @@
+defmodule Brain.FileFind do
+  @moduledoc """
+  Search for text in files.
+  
+  @ignorewords contains a list of words to ignore.
+
+  ## TODO list:
+
+  ### V. 0.3
+  * Use Flow to do the indexing
+  
+  ### FAR into the future
+  * Keep track of neighbors
+  * Build a graph
+  * Get fancy.
+  """
+
+  @ignorewords [
+    "de",
+    "het",
+    "een",
+    "die",
+    "dat",
+    "deze",
+    "dit",
+    "om",
+    "te",
+    "the",
+    "a",
+    "an",
+    "this",
+    "that",
+    "to",
+  ]
+  
+  @doc """
+  Return a term index (term => number of occurences) for the given file. The term index is a map with the number
+  of occurences for each term.
+  
+  Throws an exception if the file can't be read.
+
+  ## Parameters
+
+  - file: the file to read
+
+  """
+  def index_file!(file) do
+    File.read!(file)
+    |> String.trim
+    |> String.downcase
+    |> String.split(~r/\W+/)
+    |> Enum.reduce(%{}, fn(word, words) -> 
+      cond do
+        word in @ignorewords -> words
+        true -> Map.update(words, word, 1, & &1 + 1) 
+      end
+      end)
+  end
+
+  @doc """
+  Return an index of all files in a given directory. The index is a map with the 
+  path of every file as the key and a map of available terms and their occurences
+  for the file.
+
+  Throws an exception if a file can't be indexed.
+
+  TODO use Flow (I think?) to parallelize indexing
+
+
+  ## Parameters
+
+  - dir: the directory to index
+  """
+  def index_dir!(dir) do
+    index_dir!(%{}, dir)
+  end
+
+  defp index_dir!(index, dir) do
+    Enum.reduce(Path.wildcard("#{dir}/*"), index, fn(path, index_in_fn) ->
+      cond do
+        File.dir?(path) -> index_dir!(index_in_fn, path)
+        # Only index txt and md files
+        File.regular?(path) && String.match?(path, ~r/\.(txt|md)$/) 
+          -> Map.put(index_in_fn, path, index_file!(path))
+        true -> index
+      end
+    end)
+
+  end
+
+  @doc """
+  Return files in the given index that match the given search term: a map and the number of occurences of the term.
+
+  Throws an error if a file can't be read.
+
+  ## Parameters
+
+  - index: the index (see index_dir) to search
+  - term: the term to search for.
+  """
+  def find_in_index(index, term) do
+    #Enum.filter(index, fn({_path, item_index}) ->
+      #find_in_index_entry(item_index, term) > 0
+    #end)
+    Enum.reduce(index, %{}, fn({path, item_index}, acc) ->
+      num_matches = find_in_index_entry(item_index, term)
+      cond do
+        num_matches > 0 -> Map.update(acc, path, num_matches, &(&1 + num_matches) ) 
+        true -> acc
+      end
+    end)
+
+  end
+
+  defp find_in_index_entry(index, term) do
+    Map.get(index, term, 0)
+  end
+
+
+
+
+
+end
