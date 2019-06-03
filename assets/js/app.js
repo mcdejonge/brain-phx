029fdc9a6de04951f56eb5165b7c04204bd4d3a6
Matthijs de Jonge <rhmtts@gmail.com>
2019-06-03 15:54:40 +0200

    enabled react

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,17 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+class HelloReact extends React.Component {
+  render() {
+    return (<h1>Hello React!</h1>)
+  }
+}
+
+ReactDOM.render( 
+  <HelloReact />,
+  document.getElementById("hello-react")
+)
